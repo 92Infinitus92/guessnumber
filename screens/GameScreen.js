d19ac9e37da959c42e9c491b9e002b142a643487
92Infinitus92 <benjaminj@dev-it.info>
2023-05-03 15:07:56 +0300

    implementing micro component strategy

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { View, Text, StyleSheet } from 'react-native';
+import Title from '../components/Title';
 
 const GameScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text style={styles.title}>Opponent's Guess</Text>
+      <Title>Opponent's Guess</Title>
       {/* Guess */}
       <View>
         <Text>Higher or Lower?</Text>
@@ -22,14 +23,5 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 36,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#ddb52f',
-    textAlign: 'center',
-    borderWidth: 2,
-    borderColor: '#ddb52f',
-    padding: 12,
   },
 });
